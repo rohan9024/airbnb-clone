997b168dc2672b1b661729e8204d0a77e6db31af
rohan9024 <rohanwandre24@gmail.com>
2023-09-24 12:36:06 +0530

    Added header and main

--- components/Header.js
+++ components/Header.js
@@ -1,73 +1,103 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SearchIcon } from "@heroicons/react/solid";
 import { GlobeAltIcon } from "@heroicons/react/solid";
 import { MenuIcon } from "@heroicons/react/solid";
 
 function Header() {
+    const [show, handleShow] = useState(false);
+    const [showSearch, setShowSearch] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 2) {
+                handleShow(true);
+            } else handleShow(false);
+        });
+
+    }, []);
+
     return (
-        <div className='bg-black '>
-            <header className='grid grid-cols-8 sticky top-0 z-50 shadow-md p-5 md:px-10 bg-black'>
-                {/* Left */}
-                <div className='col-span-2 relative flex items-center h-10 cursor-pointer my-auto'>
-                    <Image
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1024px-Airbnb_Logo_B%C3%A9lo.svg.png"
-                        layout='fill'
-                        objectFit='contain'
-                        objectPosition="left"
-                    />
-                </div>
-                {/* Mid  */}
-                <div className='col-span-4 flex-col flex '>
-                    <div className='flex text-lg font-normal justify-center space-x-12 items-center flex-grow'>
-                        <h1 className='text-white '>Places to Stay</h1>
-                        <h1 className='text-white '>Experiences</h1>
-                        <h1 className='text-white '>Online Experiences</h1>
+        <div className='bg-black flex flex-col'>
+            <div className=''>
+                <header className={`grid grid-cols-8 fixed top-0 z-50 shadow-md p-5 md:px-10 w-full bg-black ${show && 'bg-white '}`}>
+                    {/* Left */}
+                    <div className='col-span-2 relative flex items-center h-10 cursor-pointer my-auto'>
+                        <Image
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1024px-Airbnb_Logo_B%C3%A9lo.svg.png"
+                            layout='fill'
+                            objectFit='contain'
+                            objectPosition="left"
+                        />
                     </div>
 
-                </div>
+                    {/* Mid */}
+                    {show ?
+                        (
+                            <div className=' w-full col-span-4 flex justify-center items-center'>
+                                <div className='flex justify-between items-center border-2 rounded-full w-64 h-12 '>
+                                    <h1 className='ml-6 font-semibold text-normal text-black'>Start your search</h1>
+                                    <SearchIcon className='hidden h-8 bg-red-500 text-white rounded-full p-2 cursor-pointer md:inline-flex md:mx-2' />
+                                </div>
+                            </div>
+                        ) :
+                        (<div className={`col-span-4 flex-col flex ${show || ' transition scale-105 ease-in-out duration-300 delay-150'} ${show && 'transition scale-0 duration-300 ease-in-out delay-150'}`}>
+                            <div className='flex text-lg font-normal justify-center space-x-12 items-center flex-grow text-white'>
+                                <h1 className=''>Places to Stay</h1>
+                                <h1 className=''>Experiences</h1>
+                                <h1 className=''>Online Experiences</h1>
+                            </div>
+                        </div>)
 
-                {/* Right */}
-                <div className='col-span-2 flex justify-evenly items-center'>
-                    <div>
-                        <h1 className='text-white font-bold tracking-tight'>Become a host</h1>
-                    </div>
-                    <div className='hover:bg-gray-600'>
-                        <GlobeAltIcon className='text-white h-5 hover:bg-gray-600 hover:rounded-full cursor-pointer' />
-                    </div>
-                    <div className='flex justify-center items-center bg-white space-x-5 p-2 rounded-full'>
-                        <MenuIcon className="h-5 w-5 text-black cursor-pointer  hover:ease-in-out hover:duration-100 hover:scale-125 space-y-0" />
-                        <img src="https://avatarfiles.alphacoders.com/161/thumb-161485.png" className="h-10 hover:ease-in-out hover:duration-100 hover:scale-125 cursor-pointer rounded-full text-black" />
-                    </div>
-                </div>
+                    }
 
-            </header>
-            <div className='flex justify-around space-x-12 items-center p-4 rounded-full bg-white w-2/3 ml-72'>
-                <div className='flex flex-col text-black'>
-                    <h1 className='flex-start text-sm font-semibold'>Location</h1>
-                    <h3 className='text-gray-500 font-thin '>Where are you going?</h3>
-                </div>
-                <div className='border-r-2 border-gray-200 h-10' />
-                <div className=' flex flex-col text-black'>
-                    <h1 className='text-sm font-semibold'>Check in</h1>
-                    <h3 className='text-gray-500 font-thin '>Add dates</h3>
-                </div>
-                <div className='border-r-2 border-gray-200 h-10' />
-                <div className=' flex flex-col text-black'>
-                    <h1 className='text-sm font-semibold'>Check out</h1>
-                    <h3 className='text-gray-500 font-thin '>Add dates</h3>
-                </div>
-                <div className='border-r-2 border-gray-200 h-10' />
-                <div className=' flex flex-col text-black'>
-                    <h1 className='text-sm font-semibold'>Guests</h1>
-                    <h3 className='text-gray-500 font-thin '>Add guests</h3>
-                </div>
-                <div className='border-r-2 border-gray-200 h-10' />
-                <div>
-                    <SearchIcon className='hidden h-10 bg-red-400 text-white rounded-full p-2 cursor-pointer md:inline-flex md:mx-2' />
-                </div>
+
+                    {/* Right */}
+                    <div className='col-span-2 flex justify-evenly items-center'>
+                        <div>
+                            <h1 className={`text-white font-semibold tracking-tighter ${show && 'text-black'}`}>Become a host</h1>
+                        </div>
+                        <div className=''>
+                            <GlobeAltIcon className={`text-white h-5 hover:animate-spin cursor-pointer ${show && 'text-black'}`} />
+                        </div>
+                        <div className='flex justify-center items-center bg-white space-x-5 p-2 rounded-full'>
+                            <MenuIcon className="h-5 w-5 text-black cursor-pointer space-y-0" />
+                            <img src="https://avatarfiles.alphacoders.com/161/thumb-161485.png" className="h-10 hover:animate-bounce cursor-pointer rounded-full text-black" />
+                        </div>
+                    </div>
+
+                </header >
             </div>
+            <div className={'bg-black'}>
+
+                <div className={show ? 'mt-24 flex justify-around space-x-12 items-center p-4 rounded-full bg-white w-2/3 ml-72 scale-0 transition ease-in-out duration-300 delay-150' : 'mt-24 flex justify-around space-x-12 items-center p-4 rounded-full bg-white w-2/3 ml-72 scale-100 transition ease-in-out duration-300 delay-150'}>
+                    <div className='flex flex-col text-black cursor-pointer '>
+                        <h1 className='flex-start text-sm font-semibold'>Location</h1>
+                        <h3 className='text-gray-500 font-thin '>Where are you going?</h3>
+                    </div>
+                    <div className='border-r-2 border-gray-200 h-10' />
+                    <div className=' flex flex-col text-black cursor-pointer'>
+                        <h1 className='text-sm font-semibold'>Check in</h1>
+                        <h3 className='text-gray-500 font-thin '>Add dates</h3>
+                    </div>
+                    <div className='border-r-2 border-gray-200 h-10' />
+                    <div className=' flex flex-col text-black cursor-pointer'>
+                        <h1 className='text-sm font-semibold'>Check out</h1>
+                        <h3 className='text-gray-500 font-thin '>Add dates</h3>
+                    </div>
+                    <div className='border-r-2 border-gray-200 h-10' />
+                    <div className=' flex flex-col text-black cursor-pointer'>
+                        <h1 className='text-sm font-semibold'>Guests</h1>
+                        <h3 className='text-gray-500 font-thin '>Add guests</h3>
+                    </div>
+                    <div className='border-r-2 border-gray-200 h-10' />
+                    <div>
+                        <SearchIcon className='hidden h-10 bg-red-400 text-white rounded-full p-2 cursor-pointer md:inline-flex md:mx-2' />
+                    </div>
+                </div>
+            </div >
         </div>
+
     );
 }
 
--- components/Main.js
+++ components/Main.js
@@ -3,16 +3,10 @@ import React from 'react';
 
 function Main() {
     return (
-        // <div className='flex justify-center items-center bg-black text-white'>
-        //     <h1 className=' text-bold z-10 text-center text-5xl	'>Not sure where to go?</h1>
-        // <img className='absolute mt-20 w-5/6 z-0' src="https://a0.muscache.com/im/pictures/53e51dcb-8fad-4ce8-b61c-8a7a369267bf.jpg?im_w=1200" alt="img" />
-
-
-        // </div>
         <div className='bg-black flex justify-center items-center'>
             <div className=' z-30 mt-96 absolute h-56 flex justify-center items-center '>
                 <div className='flex-col space-y-12'>
-                    <div className='flex justify-center items-center flex-col text-white'>
+                    <div className='flex justify-center items-center flex-col text-white transitition hover:scale-0 ease-in-out delay-200 duration-500'>
                         <h1 className='text-5xl font-bold md:text-5xl '>Not sure where to go? Perfect.</h1>
                     </div>
                   
